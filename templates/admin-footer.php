db22953ab468d39a7f3136586085be8dd0518a57
ecommnet <ecommnet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 01:22:39 +0000

    version 0.3
    git-svn-id: https://plugins.svn.wordpress.org/seo-checker/tags/0.3@1077033 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/admin-footer.php
@@ -0,0 +1,8 @@
+<?php defined('ABSPATH') or die('Plugin file cannot be accessed directly.'); ?>
+
+<div class="ecommnet-footer">
+	<a target="_blank" href="http://www.ecommnet.uk/">
+		<img src="<?php echo SEO_Checker()->plugin_url() ?>/images/logo.png" alt="p"/>
+	</a>
+	<p class="seo-checker">This plugin is made by Ecommnet Ltd, we provide WordPress solutions, web development and design, security consultancy and more. <a target="_blank" href="http://www.ecommnet.uk/">Click here</a> to find out more about our services.</p>
+</div>
---
+++ templates/admin-info.php
@@ -0,0 +1,3 @@
+<?php defined('ABSPATH') or die('Plugin file cannot be accessed directly.'); ?>
+
+<p class="seo-checker">The purpose of this simple plugin is to display a clear warning when search engines are being blocked and sends regular email reminders to the site admin.</p>
